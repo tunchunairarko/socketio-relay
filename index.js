6e407b635a6783f0305945331ba97212e6223c63
tunchunairarko <tunchunairarko@gmail.com>
2021-06-10 13:49:12 +0200

    g

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const helmet = require("helmet");
+const socketio = require("socket.io")
+import io from "socket.io-client"
+const socketioclient = io("http://localhost:9000")
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// app.use(express.static("client/build"))
+const PORT = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === 'OPTIONS') {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+      return res.status(200).json({});
+  }
+  next();
+});
+
+// set up routes
+
+
+const server = app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+/////////////////////
+//////////////////////
+///////////////////
+
+
+
+const io = socketio(server,{pingTimeout: 0, origins:"*:*",allowEIO3: true})
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("pythondata",function(frame){
+    
+    var buff = Buffer.from(frame).toString()
+    let base64data = buff.toString('base64');
+   
+    socketioclient.emit("FROMPYAPI",base64data)
+    
+  })
+  
+
+  socket.on("frontenddata",function(data){
+    console.log(data)
+    socketioclient.emit("FROMNODEAPI",data)
+  })
+
+  socket.on("frontendspeechdata",function(data){
+    console.log(data)
+    socketioclient.emit("FROMNODESPEECHAPI",data)
+  })
+
+  socket.on("remoterobotdata",function(data){
+    console.log(data)
+    
+    socketioclient.emit("FROMREMOTEROBOT",data)
+    
+  })
+  socket.on("ANIMUSFPS",function(data){
+    console.log(data)
+    socketioclient.emit("FPSDATA",data)
+  })
+  socket.on("PEPPERBATTERY",function(data){
+    console.log(data)
+    socketioclient.emit("BATTERYDATA",data)
+  })
+  socket.on("PEPPERGAZE",function(data){
+    console.log(data)
+    socketioclient.emit("GAZEDATA",data)
+  })
+  socket.on("PEPPERSONAR",function(data){
+    console.log(data)
+    socketioclient.emit("SONARDATA",data)
+  })
+  socket.on("PEPPERCONTEST",function(data){
+    console.log(data)
+  })
+
+});
+
+
